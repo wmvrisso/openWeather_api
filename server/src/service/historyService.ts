ad87483775c9066cc9376dacf8ad77367aee9d03
wmvrisso <wmvrisso@gmail.com>
2025-02-01 20:04:50 -0800

    update1 historyService.ts file

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,17 +1,51 @@
 // TODO: Define a City class with name and id properties
+class City {
+  name: string;
+  id: string;
+
+  constructor(name: string, id: string) {
+    this.name = name;
+    this.id = id;
+  }
+}
 
 // TODO: Complete the HistoryService class
 class HistoryService {
   // TODO: Define a read method that reads from the searchHistory.json file
   // private async read() {}
+  private async read() {
+    return await FileSystem.readfile('db/db.json', {
+      flag: 'a+',
+      encoding: 'utf8',
+    });
+  }
+
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
   // private async write(cities: City[]) {}
+  private async write(cities: City[]) {
+    return await FileSystem.writefile('db/db.json', JSON.stringify(cities, null, '\t'));
+  }
+  
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
   // async getCities() {}
+  async getCities() {
+    return await this.read().then((cities) => {
+      let parsedCities: City[];
+  
   // TODO Define an addCity method that adds a city to the searchHistory.json file
   // async addCity(city: string) {}
+  async addCity(city: string) {
+    if (!city) {
+      throw new Error('City name is required');
+    }
+  
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
   // async removeCity(id: string) {}
+  async removeCity(id: string) {
+    return await this.getCities()
+      .then((cities) => cities.filter((city) => city.id !== id))
+      .then((filterCities) => this.write(filterCities));
+  }
 }
 
 export default new HistoryService();
