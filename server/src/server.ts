ea5bddbe2a9a5c83600a96d84d6f83fb04c14277
wmvrisso <wmvrisso@gmail.com>
2025-02-01 20:43:06 -0800

    update1 to server.ts file

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,8 +10,10 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
+app.use(express.static('client/dist'));
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
+app.use((express.json(), express.urlencoded({ extended: true })));
 
 // TODO: Implement middleware to connect the routes
 app.use(routes);
